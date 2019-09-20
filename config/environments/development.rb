2ba7c7e423a3a72456e440a69a4b31a296d6a96d
yoshiki chiba <yoshiki.chiba@one-star.jp>
2019-09-20 15:39:36 +0900

    config周りの初期設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,17 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Bullet の設定
+  config.after_initialize do
+    Bullet.enable = true # Bulletプラグインを有効
+    Bullet.alert = true # JavaScriptでの通知
+    Bullet.bullet_logger = true # log/bullet.logへの出力
+    Bullet.console = true # ブラウザのコンソールログに記録
+    Bullet.rails_logger = true # Railsログに出力
+  end
+
+  # ログの整形
+  config.logger = Logger.new(config.paths['log'].first)
+  config.logger.formatter = Logger::Formatter.new
 end
